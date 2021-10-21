b4eebd029d38ea1a59ab1b40940f38d461881f31
npbtien1100 <48831732+npbtien1100@users.noreply.github.com>
2021-10-21 15:18:20 +0700

    set up express, mysql, sequelize

---
+++ components/classes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+import {} from "./classController";
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+router.get();
+router.post();
+router.put();
+router.delete();
+
+module.exports = router;
